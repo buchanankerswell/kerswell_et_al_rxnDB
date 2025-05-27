f6fa487ac0f4277c63d4e49783e19bf035ba6816
Buchanan Kerswell <buck.kerswell@gmail.com>
2025-05-27 18:57:54 +0100

    27 may 2025 update; fix formatted string syntax for python 3.10 and 3.11

--- rxnDB/app.py
+++ rxnDB/app.py
@@ -650,9 +650,7 @@ def server(input: Inputs, output: Outputs, session: Session) -> None:
         """Show plot settings info"""
         data_type = f"Data type: {_rv_toggle_data_type()}\n"
         similar_reactions = f"Similar rxns: {_rv_toggle_similar_reactions()}\n"
-        table_selections = f"Table selections:\n{'\n'.join(_rv_selected_table_rows())
-            if _rv_selected_table_rows()
-            else "None"}"
+        table_selections = f"Table selections:\n{'\n'.join(_rv_selected_table_rows()) if _rv_selected_table_rows() else 'None'}"
 
         info = data_type + similar_reactions + table_selections
 
